dc093c38971bf1ee96d6ed69ec65ddb03985d71c
Tomasz <tomasz.debek@connectmedica.com>
2014-09-18 11:20:40 +0200

    Przy update zmiana na: <%= form_for @post, :html => { :multipart => true }  do |f| %>

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ActiveRecord::Base
+  has_attached_file :avatar
+  has_attached_file :avatar1
+  has_attached_file :avatar2
 
   belongs_to :user
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
@@ -9,6 +12,19 @@ class Post < ActiveRecord::Base
 
   has_attached_file :avatar2, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar2, :content_type => /\Aimage\/.*\Z/
+
+
+  validates_attachment_content_type :avatar, :content_type => /\Aimage/
+  validates_attachment_file_name :avatar, :matches => [/png\Z/, /jpe?g\Z/]
+  do_not_validate_attachment_file_type :avatar
+
+  validates_attachment_content_type :avatar1, :content_type => /\Aimage/
+  validates_attachment_file_name :avatar1, :matches => [/png\Z/, /jpe?g\Z/]
+  do_not_validate_attachment_file_type :avatar1
+
+  validates_attachment_content_type :avatar2, :content_type => /\Aimage/
+  validates_attachment_file_name :avatar2, :matches => [/png\Z/, /jpe?g\Z/]
+  do_not_validate_attachment_file_type :avatar2
 
 
 
