17a8ba7dba8f2ec661eb9fc89ead6ec05b8842f9
Tomasz <tomasz.debek@connectmedica.com>
2014-09-18 10:33:52 +0200

    Nowe migracje, wiecej zdjec na stronie

---
+++ db/migrate/20140918082134_add_avatar1_columns_to_posts.rb
@@ -0,0 +1,13 @@
+class AddAvatar1ColumnsToPosts < ActiveRecord::Migration
+
+  def self.up
+    add_attachment :posts, :avatar1
+  end
+
+  def self.down
+    remove_attachment :posts, :avatar1
+  end
+
+
+
+end
