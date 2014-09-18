b11d9e1e95039a64463cbdb2aef6d46e21c55aff
Tomasz <tomasz.debek@connectmedica.com>
2014-09-18 12:15:32 +0200

    Po kliknieciu na zdjecie przeskakuja nam na wiekszy rozmiar<div><%= link_to image_tag(@post.avatar2.url(:medium)), @post.avatar2.url  %></div><br>

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,5 @@
 class Post < ActiveRecord::Base
   has_attached_file :avatar
-  has_attached_file :avatar1
-  has_attached_file :avatar2
 
   belongs_to :user
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
@@ -14,18 +12,9 @@ class Post < ActiveRecord::Base
   validates_attachment_content_type :avatar2, :content_type => /\Aimage\/.*\Z/
 
 
-  validates_attachment_content_type :avatar, :content_type => /\Aimage/
-  validates_attachment_file_name :avatar, :matches => [/png\Z/, /jpe?g\Z/]
-  do_not_validate_attachment_file_type :avatar
-
-  validates_attachment_content_type :avatar1, :content_type => /\Aimage/
-  validates_attachment_file_name :avatar1, :matches => [/png\Z/, /jpe?g\Z/]
-  do_not_validate_attachment_file_type :avatar1
-
-  validates_attachment_content_type :avatar2, :content_type => /\Aimage/
-  validates_attachment_file_name :avatar2, :matches => [/png\Z/, /jpe?g\Z/]
-  do_not_validate_attachment_file_type :avatar2
-
+  def avatar_from_url(url)
+    self.picture = open(url)
+  end
 
 
 end
